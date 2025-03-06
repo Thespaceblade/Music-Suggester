b7e6504474a6b90dc6175efe1a9ddcde56db7c0a
Jason Charwin <jasoncharwin05@gmail.com>
2025-03-05 19:08:14 -0500

    Delete ML Project Music/import pandas as pd.py

--- ML Project Music/import pandas as pd.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-file_path = '/Users/jasoncharwin/Personal Academic/Personal Code Projects/ML Project Music/music_data.csv'
-df = pd.read_csv(file_path)
-
-print(df.head())
-print(df["Sentiment"].value_counts())
